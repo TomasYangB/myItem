67caacb65fd71812f592f8eb4fcf1f34644a695f
TomasYangB <scjy@yangbo.local>
2016-03-04 10:07:01 +0800

    init

---
+++ myItem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myItem
+//
+//  Created by bobo on 16/3/4.
+//  Copyright © 2016年 杨博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myItem/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  myItem
+//
+//  Created by bobo on 16/3/4.
+//  Copyright © 2016年 杨博. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+//    1.0版已经完成
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
